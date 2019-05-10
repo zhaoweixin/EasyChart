5707892a338a3609005340752c4b6a6e62a0130f
DCMagic <1247757314@qq.com>
2019-05-10 09:16:21 +0800

    dash
    dash

--- client/src/store/getter.js
+++ client/src/store/getter.js
@@ -1,5 +1,7 @@
 export default {
     someLists: state => param => {
         return state.someLists.filter(() => param.done)
-    }
+    },
+    
+
 }
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -10,8 +10,410 @@ import mutations from "./mutation"
 Vue.use(Vuex)
 
 const state = {
-  count: 0,
-  someLists: []
+  // count: 0,
+  // someLists: [],
+  // chartId:"", //图表id
+  // //chartName:chartName,  //图表名称(类型)
+  // chartComponentName: "", //图表组件名称
+  // chartlayout: myLayout,  //1.图表的布局
+  // configuration: myConfiguration, //2.图表的配置
+  // //coordinate: [x_axis,y_axis],  //坐标轴
+  // chartData: data,  //3.图表数据
+  // //legend: legend, //图示
+  // //tooltip: tip, //提示
+  // interaction: myAction,  //4.交互
+  // size: { //图表样式
+  //   width: width,  //图表的宽
+  //   height: height,  //图表的长
+
+  // },
+  /*x_axis: { //横坐标轴
+    title: Xtitle,  //横坐标名称
+    label: XLabel,  //横坐标标签
+  },
+  y_axis: { //纵坐标轴
+    title: Ytitle,  //纵坐标名称
+    label: YLabel,  //纵坐标标签
+  },
+  label: {
+    offset: chartoffset,  //标签的偏移量
+    text: {
+      fontSize: textFontSize, //字体大小
+      color:  textColor,  //字体颜色
+    }
+
+  },
+  data:[],   //数据
+  gaugecoor: {
+    startAngle: 0,  //起始角度
+    endAngle: 0,  //终止角度
+    innerRadius: 0, //内半径
+    radius: 0,  //半径
+  }*/
+
+  // myLayout: {
+  //   size: size, //图标大小的设置
+  //   padding: [0,0,0,0], //内边距
+
+  // },
+  // myConfiguration: {
+  //   // 根据不同图表进行配置
+  //   legend: false,  //图示
+  //   tooltip: false, //提示
+  //   axis: {
+  //     x_axis: false,  //x轴
+  //     y_axis: false,  //y轴
+  //   },
+
+  // },
+  // data: [],
+  // myAction: {
+
+  // },
+//bubblechart
+  bubblechart: {
+    "id": "001",
+    "category": "bubblechart", //种类
+    "container": {  //布局
+      "height": "100",
+      "width": "100",
+      "padding": [0,0,0,0]
+    },
+    "meta": {
+      "title":"bubblechart",
+      "legend":false, //图示
+      "tooltip": "", //提示
+      "X_axis": {},
+      "Y_axis": {},
+      "value": {
+        "size": [],
+        "color": "red",
+        "label": {},
+        "opacity": "0.3",
+        "shape": "circle",
+        "style": {},
+      },
+    },
+    "data":[],
+    "interaction": {
+
+    }
+  },
+//bubble-frequency-chart
+  bubbleFrequencyChart: {
+    "id": "002",
+    "category": "bubbleFrequencyChart", //种类
+    "container": {  //布局
+      "height": "100",
+      "width": "100",
+      "padding": [0,0,0,0]
+    },
+    "meta": {
+      "title":"bubbleFrequencyChart",
+      "legend": false,
+      "tooltip":"",
+      "X_axis": {},
+      "Y_axis": {},
+      "value": {
+        "size": [],
+        "color": "blue",
+        "label": {},
+        "opacity": "0.3",
+        "shape": "circle",
+        "style": {},
+      },
+      "data": [],
+      "interaction": {}
+    } 
+  },
+//gaugechart
+  gaugechart: {
+    "id": "003",
+    "category": "gaugechart", //种类
+    "container": {  //布局
+      "height": "100",
+      "width": "100",
+      "padding": [0,0,0,0],
+    },
+    "meta": {
+      "title": "gaugechart",
+      "legend": false,
+      "tooltip": "",
+      "axis": false,
+      "coor": {
+        "startAngle": "0",
+        "endAngle": "180",
+        "innerRadius":"0.75",
+        "radius": "0.8",
+      }
+    },
+    "data": [],
+    "interaction": {
+
+    }
+  },
+//groupbarchart
+  groupBarChart: {
+    "id": "004",
+    "category": "groupBarChart",
+    "container": { //布局
+      "height": "100",
+      "width": "100",
+      "padding": [0,0,0,0],
+      
+    },
+    "meta": {
+      "title": "groupBarChart",
+      "legend": false,
+      "tooltip": "",
+      "axis": {
+        "label": {
+
+        },
+        
+      },
+      "coor": {
+        "startAngle": "0",
+        "endAngle": "180",
+        "innerRadius":"0.75",
+        "radius": "0.8",
+      }
+    },
+    "data": [],
+    "intercation": {
+
+    }
+  },
+//groupPieChart
+  groupPieChart: {
+    "id": "005",
+    "category": "groupPieChart",
+    "container": { //布局
+      "height": "100",
+      "width": "100",
+      "padding": [0,0,0,0],
+      "forceFit": true,
+      
+    },
+    "meta": {
+      "title": "groupPieChart",
+      "legend": false,
+      "tooltip": "",
+      "axis": {
+        "label": {
+
+        },
+        
+      },
+      "facet": {
+        "fields": [],
+        "padding": 10,
+        "rowTitle": null,
+        "rows": 6,
+        "cols": 1,
+        "colTitle": null,
+        "eachView": {
+          "percent": {},
+          "group": {},
+          "coor": {},
+          "position": {}
+        }
+      }
+    },
+    "data": [],
+    "intercation": {
+      
+    }
+  },
+//LineChart
+  lineChart: {
+    "id": "006",
+    "category": "LineChart", //种类
+    "container": {  //布局
+      "height": "100",
+      "width": "100",
+      "padding": [0,0,0,0],
+    },
+    "meta": {
+      "title": "LineChart",
+      "legend": false,
+      "tooltip": "",
+      "axis": {
+        "x_axis": {
+          "label": {}
+        },
+        "y_axis": {
+          "label": {}
+        },
+      },
+      "coor": {
+        "startAngle": "0",
+        "endAngle": "180",
+        "innerRadius":"0.75",
+        "radius": "0.8",
+      },
+      "crosshairs": {
+        "type":"line"
+      },
+      "stroke": "",
+      "line": 1,
+    },
+    "data": [],
+    "interaction": {
+
+    }
+  },
+//mapBoxView
+  mapBoxView: {
+    "id": "007",
+    "category":"mapBoxView",
+    "container": {  //布局
+      "height": "100",
+      "width": "100",
+      "padding": [0,0,0,0],
+    },
+    "meta": {
+      "title": "mapBoxView",
+      "type": "",
+      "source": {
+        "type": "",
+        "features":{
+
+        }
+      },
+      "point": {}
+    }
+  },
+//pieChart
+  pieChart: {
+    "id": "008",
+    "category": "pieChart",
+    "container": { //布局
+      "height": "100",
+      "width": "100",
+      "padding": [0,0,0,0],
+      
+    },
+    "meta": {
+      "title": "pieChart",
+      "legend": false,
+      "tooltip": "",
+      "axis": false,
+      "percent": {
+
+      },
+      "coor": "",
+      "label":
+    },
+    "data": [],
+    "intercation": {
+
+    }
+  },
+//radarChart
+  radarChart: {
+    "id": "009",
+    "category": "radarChart",
+    "container": { //布局
+      "height": "100",
+      "width": "100",
+      "padding": [0,0,0,0],
+      
+    },
+    "meta": {
+      "title": "radarChart",
+      "legend": false,
+      "tooltip": "",
+      "transform": {
+        "type": "",
+        "fields": [],
+        "key": "",
+        "value": "",
+      },
+      "score": {
+        "min": 0,
+        "max": 100,
+      },
+      "coor": {
+
+      },
+      "axis": {
+        "x_axis": {
+          "line": "",
+          "tickline": "",
+          "grid": {
+          
+          }
+        },
+        "y_axis":{
+          "line": "",
+          "tickline": "",
+          "grid": {
+          
+          }
+        }
+      },
+      "lineWidth": 1,
+      "fillOpacity": 1
+
+    }
+  },
+//ratioChart
+ratiochart: {
+  "id": "010",
+  "category": "ratioChart", //种类
+  "container": {  //布局
+    "height": "100",
+    "width": "100",
+    "padding": [0,0,0,0],
+  },
+  "meta": {
+    "title": "ratioChart",
+    "legend": false,
+    "tooltip": "",
+    "axis": {
+      "label": {
+        
+      }
+    },
+    "percent": {}
+  },
+  "data": [],
+  "interaction": {
+
+  }
+}
+
+
+//linechart example
+// option = {
+//   "id": "id1",
+//   "style": {
+//       "width": "200px",
+//       "height": "200px",
+//       "color": "orange",
+//       ...
+//   },
+//   "data": [[1,2],[1,2],[1,2],[1,2]],
+//   "meta": {
+//       //enhenced understanding information
+//       "title": "linechart",
+//       "legend": "",
+//       ...
+//   },
+//   "api": {
+//       //interaction
+//       "acceptable": {
+//           "x": limit,
+//           "y": limit
+//       },
+//       "available": {
+//           "x": limit,
+//           "y": limit
+//       }
+//   }
+//   ...
+// }
 }
 
 export default new Vuex.Store({
@@ -23,4 +425,4 @@ export default new Vuex.Store({
     //cart,
     //products
   }
-})
+}) 
