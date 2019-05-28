e61a503aa6fcc730a9dad23bf478c42f22ad46a9
Tyeyu <905109256@qq.com>
2019-05-28 19:47:38 +0800

    addDragData

--- client/src/components/ChartSide/chartStore.vue
+++ client/src/components/ChartSide/chartStore.vue
@@ -34,73 +34,84 @@
         </el-carousel-item>
       </el-carousel>
     </div>
+    <div>
+      <a>Data Drag</a>
+      <div id="staticdata">
+        <ul></ul>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import stores from "../../store/store.js";
 import mutations from "../../store/mutation.js";
 import $ from "jquery";
+import * as d3 from "d3";
+
 require("webpack-jquery-ui");
 require("webpack-jquery-ui/css");
-// console.log(stores.state.chartCategory);
-// var DefaultJson = {
-//   ParentId: "container",
-//   data: [120, 200, 150, 80, 70, 110, 130],
-//   xAxist: {
-//     type: "category",
-//     data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-//   },
-//   yAxist: {
-//     type: "value",
-//     data: null
-//   }
-// };
-// var BarEchartJson = {
-//   ToolType: "Echart",
-//   ParentId: null,
-//   ChartType: "bar",
-//   xAxist: {
-//     type: "category",
-//     data: null
-//   },
-//   yAxist: {
-//     type: "value",
-//     data: null
-//   },
-//   data: null,
-//   Style: null
-// };
-// var LineEchartJson = {
-//   ToolType: "Echart",
-//   ParentId: "container",
-//   ChartType: "line",
-//   xAxist: {
-//     type: "category",
-//     data: null
-//   },
-//   yAxist: {
-//     type: "value",
-//     data: null
-//   },
-//   data: null,
-//   Style: null
-// };
-// var PieEchartJson = {
-//   ToolType: "Echart",
-//   ParentId: "container",
-//   ChartType: "pie",
-//   xAxist: {
-//     type: "category",
-//     data: null
-//   },
-//   yAxist: {
-//     type: "value",
-//     data: null
-//   },
-//   data: null,
-//   Style: null
-// };
-// var JsonArry = [BarEchartJson, LineEchartJson, PieEchartJson];
+$.extend({
+  //读取指定csv文件的字段名
+  csvtitle: function(url, f) {
+    $.get(url, function(record) {
+      record = record.split(/\n/);
+      var title = record[0].split(",");
+      f.call(this, title);
+    });
+  }
+});
+$.csvtitle("../../../static/data/testdata.csv", function(datas) {
+  add(datas, "data1");
+});
+$.csvtitle("../../../static/data/ctestdata.csv", function(datas) {
+  add(datas, "data2");
+});
+//name为表名，或者state存储的data数据昵称
+function add(data, name) {
+  var s = name;
+  var x = 0;
+  var adds = d3
+    .select("#staticdata ul")
+    .append("li")
+    .attr("class", "dropdown");
+  adds
+    .append("a")
+    .attr("data-toggle", "dropdown")
+    .text(name)
+    .append("i")
+    .attr("class", "icon-arrow");
+  //将csv数据中的字段显示出来
+  adds
+    .append("ul")
+    .attr("class", "dropdown-menu")
+    .selectAll("li")
+    .data(data)
+    .enter()
+    .append("li")
+    .append("a")
+    .text(function(d) {
+      return d;
+    });
+  $("#staticdata ul")
+    .children()
+    .draggable({
+      helper: "clone",
+      scope: "ss",
+      stop: function() {
+        var left =
+          $("#staticdata .ui-draggable-dragging").position().left -
+          $(".el-aside").width() -
+          $(".el-main").width();
+        var top = $("#staticdata .ui-draggable-dragging").position().top;
+        console.log(left + "," + top);
+        if (left > 0 && top > 0) {
+          //判断是否在合法区域
+          console.log($(this).children()[0].text); //设置state中data的数据
+        }
+      }
+    });
+  clickdata();
+}
 var imgArray = [
   {
     id: 0,
@@ -110,12 +121,12 @@ var imgArray = [
   {
     id: 1,
     idView: require("../../../static/Image/linechart.png"),
-    chartType: "linechart"
+    chartType: "lineChart"
   },
   {
     id: 2,
     idView: require("../../../static/Image/piechart.png"),
-    chartType: "piechart"
+    chartType: "pieChart"
   }
 ];
 export default {
@@ -134,8 +145,6 @@ export default {
         .css("width", "200px")
         .css("top", function() {
           var y = $(e)[0].clientY - 80 + "px";
-          // console.log($(e).clientY);
-          // console.log(y);
           return y;
         })
         .css("left", "15%");
@@ -154,15 +163,21 @@ export default {
           var left =
             $(".el-main .ui-draggable-dragging").position().left -
             $(".el-aside").width();
-          $(".el-main .ui-draggable-dragging").css("z-index", function() {
-            if (top < 0) {
-              return -1;
+          d3.select(".el-main .ui-draggable-dragging").style(
+            "z-index",
+            function() {
+              if (top < 0 || left < 0) {
+                return -1;
+              }
+              if (
+                left + $(".el-main .ui-draggable-dragging").width() >
+                maxleft
+              ) {
+                return -1;
+              }
+              return 1;
             }
-            if (left + $(".el-main .ui-draggable-dragging").width() > maxleft) {
-              return -1;
-            }
-            return 1;
-          });
+          );
         },
         helper: "clone",
         scope: "ss",
@@ -177,177 +192,55 @@ export default {
             id: "001"
           };
           mutations.addIdToArray(stores.state, data);
-          mutations.setChartXY(stores.state,{
-            x:top,y:left
-          })
-          console.log(stores.state.chartIdArray[0].id);
-          //there use state function
-          /**
-           * function state (top,left,chartType,type)
-           *
-           * top and left is chart position
-           * type:tool type
-           */ 
-          
-          console.log(top + "," + left);
+          mutations.setChartXY(stores.state, {
+            x: top,
+            y: left
+          });
         }
       });
     },
     deletClone: function(e) {
       $("#clone").remove();
-    },
-    // clickImg: function(id) {
-    //   var ChartJson = JsonArry[id];
-    //   ChartJson = AnalysisJson(ChartJson);
-    //   console.log(ChartJson);
-    //   var mychart = this.InitEchart(ChartJson.ParentId);
-    //   if (mychart != null) {
-    //     if (ChartJson.ChartType != "pie")
-    //       this.DrawLineAndBarEchart(mychart, ChartJson);
-    //     else {
-    //       this.DrawPieChart(mychart, ChartJson);
-    //     }
-    //   }
-    //   console.log();
-    // },
-    // InitEchart: function(Chartparent) {
-    //   if (Chartparent == null) {
-    //     alert("error");
-    //     return;
-    //   }
-
-    //   var echarts = require("echarts");
-    //   var dom = document.getElementById(Chartparent);
-    //   var myChart = echarts.init(dom);
-    //   return myChart;
-    // },
-    // DrawLineAndBarEchart: function(myChart, ChartJson) {
-    //   if (myChart == null || ChartJson == null) {
-    //     alert("error");
-    //     return;
-    //   }
-    //   var option = null;
-    //   option = {
-    //     xAxis: {
-    //       type: "category",
-    //       data: ChartJson.xAxist.data
-    //     },
-    //     yAxis: {
-    //       type: "value",
-    //       data: ChartJson.yAxist.data
-    //     },
-    //     series: [
-    //       {
-    //         data: ChartJson.data,
-    //         type: ChartJson.ChartType
-    //       }
-    //     ]
-    //   };
-    //   if (option && typeof option === "object") {
-    //     myChart.setOption(option, true);
-    //   }
-    // },
-    // DrawPieChart: function(myChart, ChartJson) {
-    //   if (myChart == null || ChartJson == null) {
-    //     alert("error");
-    //     return;
-    //   }
-    //   var option = null;
-    //   option = {
-    //     series: [
-    //       {
-    //         data: ChartJson.data,
-    //         type: ChartJson.ChartType
-    //       }
-    //     ]
-    //   };
-    //   if (option && typeof option === "object") {
-    //     myChart.setOption(option, true);
-    //   }
-    // },
-    // start: function(e) {
-    //   //鼠标左键点击
-    //   if (e.button == 0) {
-    //     this.canDrag = true;
-    //     //记录鼠标指针位置
-    //     this.x0 = e.clientX;
-    //     this.y0 = e.clientY;
-    //     console.log(e);
-    //   }
-    // },
-    //   stop: function(e) {
-    //     console.log(window.document.querySelector("#app main").offsetLeft);
-    //     console.log(window.document.querySelector("#app main").offsetTop);
-    //     console.log(window.document.querySelector("#app main").scrollHeight);
-    //     console.log(window.document.querySelector("#app main").scrollWidth);
-    //     this.canDrag = false;
-    //     console.log(e);
-    //   },
-    //   move: function(e) {
-    //     if (this.canDrag == true) {
-    //       this.x1 = e.clientX;
-    //       this.y1 = e.clientX;
-    //       let x = this.x1 - this.x0;
-    //       let y = this.y1 - this.y0;
-    //       let img = document.querySelector("#Store img");
-    //       this.style = `left:${img.offsetLeft + x}px;top:${img.offsetTop +
-    //         y}px;opacity:1`;
-    //       this.x0 = this.x1;
-    //       this.y0 = this.y1;
-    //     }
-    // }
+    }
   },
-  // mounted(){
-  //   this.show();
-  // }
+  watch: {}
 };
-// function AnalysisJson(ChartJson) {
-//   if (ChartJson == null) {
-//     alert("error");
-//     return;
-//   }
-//   if (ChartJson.ParentId == null) ChartJson.ParentId = DefaultJson.ParentId;
 
-//   if (ChartJson.data == null) {
-//     if (ChartJson.ChartType == "pie") {
-//       var data = [];
-//       for (var i = 0; i < DefaultJson.data.length; i++) {
-//         var child = {
-//           value: null,
-//           name: null
-//         };
-//         child.value = DefaultJson.data[i];
-//         child.name = DefaultJson.xAxist.data[i];
-//         data.push(child);
-//       }
-//       ChartJson.data = data;
-//     } else {
-//       ChartJson.data = DefaultJson.data;
-//       if (ChartJson.xAxist.data == null && ChartJson.yAxist.data == null) {
-//         ChartJson.xAxist.data = DefaultJson.xAxist.data;
-//         ChartJson.yAxist.data = DefaultJson.yAxist.data;
-//       }
-//     }
-//   }
+function clickdata() {
+  var dropdown = document.querySelectorAll(".dropdown");
+  var dropdownArray = Array.prototype.slice.call(dropdown, 0);
+  dropdownArray.forEach(function(el) {
+    var button = el.querySelector('a[data-toggle="dropdown"]'),
+      menu = el.querySelector(".dropdown-menu"),
+      arrow = button.querySelector("i.icon-arrow");
 
-//   return ChartJson;
-// }
-// var test = {
-//   testmethods: [
-//     {
-//       LineAndBar: function(d) {
-//         console.log(d);
-//         return d;
-//       },
-//       testtwo: function(d) {
-//         return d;
-//       }
-//     }
-//   ]
-// };
-// console.log(test.testmethods[0].testtwo(2));
+    button.onclick = function(event) {
+      if (!menu.hasClass("show")) {
+        menu.classList.add("show");
+        menu.classList.remove("hide");
+        arrow.classList.add("open");
+        arrow.classList.remove("close");
+        event.preventDefault();
+      } else {
+        menu.classList.remove("show");
+        menu.classList.add("hide");
+        arrow.classList.remove("open");
+        arrow.classList.add("close");
+        event.preventDefault();
+      }
+    };
+  });
+  Element.prototype.hasClass = function(className) {
+    return (
+      this.className &&
+      new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className)
+    );
+  };
+}
 </script>
+
 <style>
+@import "../../../static/css/selectData.css";
 #Store img {
   width: 100px;
 }
