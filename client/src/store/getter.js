888ef97887e3adeb77c5329ccce3062ca94becef
Your NameRongRongLee <1406499403@qq.com>
2019-06-03 18:58:05 +0800

    change data

--- client/src/store/getter.js
+++ client/src/store/getter.js
@@ -16,14 +16,10 @@ const getter = {
     },
     rSideData: state=>{
         return state.rSideData
-<<<<<<< HEAD
     },
   getPropsData: state => {    //野
     return state.propsData
   }
-=======
-    }
->>>>>>> c847ce60e6c39c430ebdc670a8d120a07b52a48b
 }
 
 export default getter
